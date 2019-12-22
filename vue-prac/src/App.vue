0a36af78f7749f52c4e13cd873874c617b5bf29b
YujinHyeon <gusdbwls9772@gmail.com>
2019-12-22 13:37:17 +0900

    axios prac

--- vue-prac/src/App.vue
+++ vue-prac/src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <sibling></sibling>
     <sibling2></sibling2>
+    <Axios></Axios>
   </div>
 </template>
 
 <script>
 import sibling from './components/sibling.vue';
 import sibling2 from './components/sibling2.vue';
+import Axios from './components/axios.vue';
 
 export default {
   components : {
     'sibling': sibling,
-    'sibling2': sibling2
+    'sibling2': sibling2,
+    'Axios': Axios
   }
   
 }
---
+++ vue-prac/src/components/axios.vue
@@ -0,0 +1,22 @@
+<template>
+  <button v-on:click="getData">프레임워크 목록 가져오기</button>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    methods: {
+        getData() {
+            axios.get('https://github.com/HyunYuJin/gotgam_pro/blob/master/frontend/package.json')
+            .then(function(response) {
+                console.log(response);
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
