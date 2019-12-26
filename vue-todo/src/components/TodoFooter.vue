c48a9293e439cfd23f665b80428f885a271a064b
YujinHyeon <gusdbwls9772@gmail.com>
2019-12-26 17:00:40 +0900

    todo 1 commit

--- vue-todo/src/components/TodoFooter.vue
+++ vue-todo/src/components/TodoFooter.vue
@@ -1,15 +1,32 @@
 <template>
-  <div>
-      footer
+  <div class="clearAllContainer">
+      <span class="clearAllBtn" @click="clearTodo">Clear All</span>
   </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    clearTodo() {
+      localStorage.clear();
+    }
+  }
 
 }
 </script>
 
 <style>
+.clearAllContainer {
+  width: 8.5rem;
+  height: 50px;
+  line-height: 50px;
+  background: #FFF;
+  border-radius: 5px;
+  margin: 0 auto;
+}
 
+.clearAllBtn {
+  color: #E20303;
+  display: block;
+}
 </style>
