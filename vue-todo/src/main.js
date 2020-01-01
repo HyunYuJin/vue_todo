dd8e60978efaaaba25ae651760a951e737a80ff3
YujinHyeon <gusdbwls9772@gmail.com>
2020-01-01 21:06:37 +0900

    Registed Vuex

--- vue-todo/src/main.js
+++ vue-todo/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { store } from './store/store'
 
 new Vue({
   el: '#app',
+  store: store, // ES6문법: store이라고 써도 된다.
   render: h => h(App)
 })
---
+++ vue-todo/src/store/store.js
@@ -0,0 +1,9 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+// Vue를 사용하는 전역에 특정 기능을 추가하고 싶을 때 사용
+Vue.use(Vuex);
+
+export const Store = new Vuex.Store({
+    // 
+});
